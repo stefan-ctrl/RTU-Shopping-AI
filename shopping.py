ee3bbb86018b3d1a2e0f58685008e484a576075c
Stefan Siber <stefan-siber@t-online.de>
2024-12-12 20:16:31 +0200

    chore: added comment header

--- shopping.py
+++ shopping.py
@@ -1,3 +1,10 @@
+######
+# This project, also referred to as "Shopping (practical task)" is part of the of course
+# "Development Methods of Applied Intelligent Software Systems(English)(1),24/25-R" at Riga Technical University.
+#
+# This project has been developed by Stefan Siber and is hereby handover for evaluation to the lecturer.
+######
+
 import csv
 import sys
 
